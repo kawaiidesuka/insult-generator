da3389071b91b079b6a9a02209cfeb091c143be7
kawaii <nyaa@openmailbox.org>
2016-01-08 10:41:39 -0500

    made it more offensive

--- sentences.py
+++ sentences.py
@@ -1,5 +1,5 @@
 from random import choice
 insulta=["dick", "knob", "fuck", "cunt", "bitch", "tard", "fag", "cock", "twat", "arse"]
 insultb=["washing", "eating", "mongering", "polishing", "cuddling", "shooting", "lewding", "whistling", "cooking", "swoggling"]
-insultc=["canoe", "potato", "tube", "sjw", "rhubarb", "goblin", "chair", "whistle", "banana", "cheese"]
+insultc=["canoe", "potato", "eggplant", "aubergine", "rhubarb", "goblin", "chair", "whistle", "banana", "cheese"]
 print("you are a " + choice(insulta) + choice(insultb)+ " " + choice(insulta) + choice(insultc))
