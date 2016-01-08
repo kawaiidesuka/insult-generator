224ccca36a0fbd8a350c21aa916fc3f7e85a5aa6
kawaii <nyaa@openmailbox.org>
2016-01-08 10:19:30 -0500

    not for children

---
+++ sentences.py
@@ -0,0 +1,5 @@
+from random import choice
+insulta=["dick", "knob", "fuck", "cunt", "bitch", "tard", "fag", "cock", "twat", "arse"]
+insultb=["washing", "eating", "mongering", "polishing", "cuddling", "shooting", "lewding", "whistling", "cooking", "swoggling"]
+insultc=["canoe", "potato", "tube", "sjw", "rhubarb", "goblin", "chair", "whistle", "banana", "cheese"]
+print("you are a " + choice(insulta) + choice(insultb)+ " " + choice(insulta) + choice(insultc))
